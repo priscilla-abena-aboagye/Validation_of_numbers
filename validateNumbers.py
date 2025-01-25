573f1ad2307bfd04968e3f6b9341a75fbf7eec44
Abena <priscillaabenaaboagye@gmail.com>
2025-01-25 10:03:23 +0000

    for statement

--- validateNumbers.py
+++ validateNumbers.py
@@ -3,6 +3,17 @@ def verify_card_number(card_number):
     card_number_reversed = card_number[-1:-5:-1]
     odd_digits = card_number_reversed[::2]
 
+    for digit in odd_digits:
+        digit = int(digit)
+        sum_of_odd_numbers += digit
+
+    sum_of_even_numbers = 0
+    even_numbers = card_number_reversed[1::2]
+
+    for even in even_numbers:
+        even = int(even)
+        sum_of_even_numbers += even
+
 
     
 def main():
