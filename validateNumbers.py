3d2a346809680791398a6ab6eae48850be5cc627
Abena <priscillaabenaaboagye@gmail.com>
2025-01-25 12:51:21 +0000

    for loop and if statement

--- validateNumbers.py
+++ validateNumbers.py
@@ -1,6 +1,6 @@
 def verify_card_number(card_number):
     sum_of_odd_numbers = 0
-    card_number_reversed = card_number[-1:-5:-1]
+    card_number_reversed = card_number[::-1]
     odd_digits = card_number_reversed[::2]
 
     for digit in odd_digits:
@@ -10,16 +10,33 @@ def verify_card_number(card_number):
     sum_of_even_numbers = 0
     even_numbers = card_number_reversed[1::2]
 
-    for even in even_numbers:
-        even = int(even)
-        sum_of_even_numbers += even
+    for digit in even_numbers:
+        digit = int(digit)
+        number = digit * 2 
+        if number >= 10:
+            number = (number // 10) + (number % 10)
+        
+        sum_of_even_numbers += number
+
+    total = sum_of_even_numbers + sum_of_odd_numbers
+    print(total)
+    return total % 10 == 0
 
 
     
 def main():
     card_number = "837-133-64"
-    card_translation = str.maketrans({"-": "", "": " "})
+    card_translation = str.maketrans({"-": " "})
     card_translated = card_number.translate(card_translation)
+
+    if not card_translated.isdigit():
+        print("Invalid Card!")
+        return
+
+    if verify_card_number(card_translated):
+        print("Valid!")
+    else:
+        print("Invalid!")
     
 
 main()
